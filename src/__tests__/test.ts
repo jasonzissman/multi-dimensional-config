f5a38fbaa12a060f5e46cc62c14a48b697c87295
Jason Z <jason.zissman@gmail.com>
2023-07-15 07:21:33 -0500

    Add prettier formatting

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,68 +1,109 @@
 import { lookUpConfigValue, ConfigNode } from '../index';
 
-test('lookUpConfigValue', () => {
-    let configuration: ConfigNode = {
-        matches: "all"
-    };
-    let parameters = {}
-    expect(lookUpConfigValue(configuration, parameters)).toBe(undefined);
-});
-
 test('lookUpConfigValue - available movies by region', () => {
+  // Configuration describing when movies are available in
+  // a certain area. The rules include multiple dimensions:
+  // year, country, local language, and city.
 
-    // Configuration describing when movies are available in
-    // a certain area. The rules include multiple dimensions:
-    // year, country, local language, and city.
-
-    const movieConfig: ConfigNode = {
-        matches: "all",
-        subGroups: [{
-            matches: [{
-                key: "year",
-                value: 2040,
-                comparator: "gt" // movie rights expire in year 2040
-            }],
-            resolvedValue: false,
-        }, {
-            matches: "all",
-            subGroups: [{
-                matches: [{
-                    key: "country",
-                    value: "usa"
-                }, {
-                    key: "country",
-                    value: "canada"
-                }, {
-                    key: "country",
-                    value: "uk"
-                }],
-                resolvedValue: true, // movie available to these countries
-                subGroups: [{
-                    // except where local rules require non-English language
-                    resolvedValue: false,
-                    matches: [{
-                        key: "local_language",
-                        value: "french"
-                    }],
-                    subGroups: [{
-                        // unless a specific city made an exception
-                        resolvedValue: true,
-                        matches: [{
-                            key: "city",
-                            value: "toronto"
-                        }]
-                    }]
-                }]
-            }]
-        }
+  const movieConfig: ConfigNode = {
+    subGroups: [
+      {
+        matches: [
+          {
+            key: 'year',
+            value: 2040,
+            comparator: 'gt' // movie rights expire in year 2040
+          }
+        ],
+        resolvedValue: false
+      },
+      {
+        subGroups: [
+          {
+            matches: [
+              {
+                key: 'country',
+                value: 'usa'
+              },
+              {
+                key: 'country',
+                value: 'canada'
+              },
+              {
+                key: 'country',
+                value: 'uk'
+              }
+            ],
+            resolvedValue: true, // movie available to these countries
+            subGroups: [
+              {
+                // except where local rules require non-English language
+                resolvedValue: false,
+                matches: [
+                  {
+                    key: 'local_language',
+                    value: 'french'
+                  }
+                ],
+                subGroups: [
+                  {
+                    // unless a specific city made an exception
+                    resolvedValue: true,
+                    matches: [
+                      {
+                        key: 'city',
+                        value: 'toronto'
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
         ]
-    };
-    expect(lookUpConfigValue(movieConfig, { year: 2023, country: "usa" })).toBe(true);
-    expect(lookUpConfigValue(movieConfig, { year: 2043, country: "usa" })).toBe(false);
-    expect(lookUpConfigValue(movieConfig, { year: 2023, country: "uk" })).toBe(true);
-    expect(lookUpConfigValue(movieConfig, { year: 2023, country: "canada" })).toBe(true);
-    expect(lookUpConfigValue(movieConfig, { year: 2023, country: "canada", local_language: "english" })).toBe(true);
-    expect(lookUpConfigValue(movieConfig, { year: 2023, country: "canada", local_language: "french" })).toBe(false);
-    expect(lookUpConfigValue(movieConfig, { year: 2023, country: "canada", local_language: "french", city: "toronto" })).toBe(true);
-    expect(lookUpConfigValue(movieConfig, { year: 2023, country: "canada", local_language: "french", city: "quebec" })).toBe(false);
+      }
+    ]
+  };
+  expect(lookUpConfigValue(movieConfig, { year: 2023, country: 'usa' })).toBe(
+    true
+  );
+  expect(lookUpConfigValue(movieConfig, { year: 2043, country: 'usa' })).toBe(
+    false
+  );
+  expect(lookUpConfigValue(movieConfig, { year: 2023, country: 'uk' })).toBe(
+    true
+  );
+  expect(
+    lookUpConfigValue(movieConfig, { year: 2023, country: 'canada' })
+  ).toBe(true);
+  expect(
+    lookUpConfigValue(movieConfig, {
+      year: 2023,
+      country: 'canada',
+      local_language: 'english'
+    })
+  ).toBe(true);
+  expect(
+    lookUpConfigValue(movieConfig, {
+      year: 2023,
+      country: 'canada',
+      local_language: 'french'
+    })
+  ).toBe(false);
+  expect(
+    lookUpConfigValue(movieConfig, {
+      year: 2023,
+      country: 'canada',
+      local_language: 'french',
+      city: 'toronto'
+    })
+  ).toBe(true);
+  expect(
+    lookUpConfigValue(movieConfig, {
+      year: 2023,
+      country: 'canada',
+      local_language: 'french',
+      city: 'quebec'
+    })
+  ).toBe(false);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,75 @@
-type Comparator = "eq" | "lt" | "lte" | "gt" | "gte";
-type ParamType = number|boolean|string;
+type Comparator = 'eq' | 'lt' | 'lte' | 'gt' | 'gte';
+type ParamType = number | boolean | string;
 type MatchingParameter = {
-    [key: string]: ParamType;
-}
+  [key: string]: ParamType;
+};
 type MatchingParameterRule = {
-    key: string;
-    value: ParamType;
-    comparator?: Comparator;
-}
+  key: string;
+  value: ParamType;
+  comparator?: Comparator;
+};
 export type ConfigNode = {
-    matches: MatchingParameterRule[] | "all";
-    resolvedValue?: any;
-    subGroups?: ConfigNode[];
-}
+  matches?: MatchingParameterRule[] | 'all';
+  resolvedValue?: any;
+  subGroups?: ConfigNode[];
+};
 
-const satisfiesCondition = (p1: MatchingParameterRule, paramValue: ParamType, comp: Comparator) => {
-    if (comp === "eq") {
-        return p1.value === paramValue;
-    } else if (comp === "lt") {
-        return paramValue < p1.value;
-    } else if (comp === "lte") {
-        return paramValue <= p1.value;
-    } else if (comp === "gt") {
-        return paramValue > p1.value;
-    } else if (comp === "gte") {
-        return paramValue >= p1.value;
-    } else {
-        return false;
-    }
-}
+const satisfiesCondition = (
+  p1: MatchingParameterRule,
+  paramValue: ParamType,
+  comp: Comparator
+) => {
+  if (comp === 'eq') {
+    return p1.value === paramValue;
+  } else if (comp === 'lt') {
+    return paramValue < p1.value;
+  } else if (comp === 'lte') {
+    return paramValue <= p1.value;
+  } else if (comp === 'gt') {
+    return paramValue > p1.value;
+  } else if (comp === 'gte') {
+    return paramValue >= p1.value;
+  } else {
+    return false;
+  }
+};
 
-const atLeastOneParamMatches = (matches: MatchingParameterRule[] | "all", parameters: MatchingParameter): boolean => {
-    return matches === "all" || !!matches.find(p1 => Object.keys(parameters).find(key => (p1.key === key && satisfiesCondition(p1, parameters[key], p1.comparator || "eq"))));
-}
+const atLeastOneParamMatches = (
+  parameters: MatchingParameter,
+  matches?: MatchingParameterRule[] | 'all'
+): boolean => {
+  return (
+    !matches ||
+    matches === 'all' ||
+    !!matches.find((p1) =>
+      Object.keys(parameters).find(
+        (key) =>
+          p1.key === key &&
+          satisfiesCondition(p1, parameters[key], p1.comparator || 'eq')
+      )
+    )
+  );
+};
 
-const lookUpConfigValueRecursive = (config: ConfigNode, parameters: MatchingParameter, depth: number): any => {
-    // TODO - if depth is beyond a configurable max, throw error
-    if (atLeastOneParamMatches(config.matches, parameters)) {
-        const matchedSubgroup = config.subGroups?.find(g => atLeastOneParamMatches(g.matches, parameters));
-        return matchedSubgroup ? lookUpConfigValueRecursive(matchedSubgroup, parameters, depth + 1) : config.resolvedValue;
-    }
-}
+const lookUpConfigValueRecursive = (
+  config: ConfigNode,
+  parameters: MatchingParameter,
+  depth: number
+): any => {
+  // TODO - if depth is beyond a configurable max, throw error
+  if (atLeastOneParamMatches(parameters, config.matches)) {
+    const matchedSubgroup = config.subGroups?.find((g) =>
+      atLeastOneParamMatches(parameters, g.matches)
+    );
+    return matchedSubgroup
+      ? lookUpConfigValueRecursive(matchedSubgroup, parameters, depth + 1)
+      : config.resolvedValue;
+  }
+};
 
-export const lookUpConfigValue = (config: ConfigNode, parameters: MatchingParameter): any => {
-    return lookUpConfigValueRecursive(config, parameters, 0);
-}
+export const lookUpConfigValue = (
+  config: ConfigNode,
+  parameters: MatchingParameter
+): any => {
+  return lookUpConfigValueRecursive(config, parameters, 0);
+};
